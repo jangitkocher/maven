0aea216101e86b48bf149883f250a636e5c3fb07
Jan Kocher <jan.kocher@hs-furtwangen.de>
2019-05-09 17:45:10 +0200

    changed testfile util

--- src/test/java/de/hfu/UtilTest.java
+++ src/test/java/de/hfu/UtilTest.java
@@ -1,5 +1,28 @@
 package de.hfu;
 
-public class UtilTest {
+import org.junit.Test;
 
+import static org.junit.Assert.*;
+
+public class UtilTest
+{
+  @Test
+  public void testIstErstesHalbjahr() 
+  {
+	  
+		  assertTrue( "message", Util.istErstesHalbjahr(1));
+		  assertTrue( "message", Util.istErstesHalbjahr(6));
+		  
+  } 
+  
+  @Test
+  public void testIstZweitesHalbjahr() 
+  {
+	  
+		  assertFalse( "Ist nicht innerhalb des zweiten Halbjahres!", Util.istErstesHalbjahr(7));
+		  assertFalse( "message", Util.istErstesHalbjahr(12));
+		  
+  } 
+  
 }
+  
